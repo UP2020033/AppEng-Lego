0ee81d469afd176df06be2bee115eff96fa0939e
UP2020033 <72713194+UP2020033@users.noreply.github.com>
2022-02-02 17:54:01 +0000

    Added server and started webpage

---
+++ src/server/index.js
@@ -0,0 +1,24 @@
+// Importing express module
+const express = require('express');
+
+// Creating the express.js server
+const app = express();
+
+// Enable serving for static pages
+app.use(express.static('../client'));
+
+// Variable to store the port value
+const portListenOn = 8080;
+
+// Listen on port
+function serverStart(portListenOn, error) {
+  app.listen(portListenOn);
+  if (!error) {
+    console.log('Server accessible on port:' + ' ' + portListenOn);
+  }
+}
+
+// Starting server
+serverStart(portListenOn);
+
+// Boakes, R. (2021). portsoc/staged-simple-message-board. GitHub. Retrieved 8 April 2021, from https://github.com/portsoc/staged-simple-message-board.
