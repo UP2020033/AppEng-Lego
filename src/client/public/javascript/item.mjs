e1696e426521f8380f3761afa0dbf1783fe29fa6
UP2020033 <72713194+UP2020033@users.noreply.github.com>
2022-03-26 14:18:59 +0000

    Added more content to item details page

--- src/client/public/javascript/item.mjs
+++ src/client/public/javascript/item.mjs
@@ -16,3 +16,19 @@ imageContainerDiv.appendChild(itemDetailImage);
 
 const itemDetailContent = pageCreate.createDiv('itemDetailContentContainer');
 containerDiv.appendChild(itemDetailContent);
+
+const itemDescriptionContainer = pageCreate.createDiv('itemDescriptionContainer');
+const itemDetailDescText = pageCreate.createParagraph('Description', 'itemDetailDescText');
+itemDetailContent.appendChild(itemDescriptionContainer);
+itemDescriptionContainer.appendChild(itemDetailDescText);
+
+const itemPriceContainer = pageCreate.createDiv('itemPriceContainer');
+const itemDetailPriceText = pageCreate.createParagraph('Price', 'itemDetail');
+itemDetailContent.appendChild(itemPriceContainer);
+itemPriceContainer.appendChild(itemDetailPriceText);
+
+const itemDetailQuantity = pageCreate.createDiv('itemQuantityContainer');
+itemDetailContent.appendChild(itemDetailQuantity);
+
+const itemAddToBasket = pageCreate.createDiv('itemAddToBasket');
+itemDetailContent.appendChild(itemAddToBasket);
