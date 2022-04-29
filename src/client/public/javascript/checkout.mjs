c0f7a22734f35e53d0d43e6f9178aace27c8357c
UP2020033 <72713194+UP2020033@users.noreply.github.com>
2022-04-29 16:33:31 +0100

    Price calculation added!

--- src/client/public/javascript/checkout.mjs
+++ src/client/public/javascript/checkout.mjs
@@ -21,7 +21,6 @@ export async function findBasketTotalPrice() {
     basketItem = JSON.parse(basketItem);
     // https://stackoverflow.com/questions/3138564/looping-through-localstorage-in-html5-and-javascript -- loop through local storage
     const quantity = basketItem.quantity;
-    console.log(quantity);
 
     const response = await fetch(`/getItemPrice/${basketItem.product_id}`);
     console.log(response);
@@ -30,18 +29,18 @@ export async function findBasketTotalPrice() {
         response.json()
           .then(details => {
             const productPrice = details[0].product_price;
-            console.log(productPrice);
             resolve(productPrice);
           });
       } catch (err) {
         reject(err);
       }
     });
-    console.log(await price);
     const itemPrice = await price;
-    console.log(`${itemPrice} x ${quantity}`);
-    totalPrice = itemPrice * quantity;
+    console.log(itemPrice);
+    console.log(quantity);
+    totalPrice = totalPrice + itemPrice * quantity;
     console.log(totalPrice);
   }
   console.log(totalPrice);
+  return totalPrice;
 }
